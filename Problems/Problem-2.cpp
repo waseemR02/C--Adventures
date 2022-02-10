26846b479b967a64451762a3e4997ecaa18fa021
Waseem <98299006+waseemR02@users.noreply.github.com>
2022-02-10 12:07:51 +0530

    Height Simulation

--- Problems/Problem-2.cpp
+++ Problems/Problem-2.cpp
@@ -32,3 +32,7 @@ int main()
 
     return 0;
 }
+
+// Write a short program to simulate a ball being dropped off of a tower. To start, the user should be asked for the height of the tower in meters. Assume normal gravity (9.8 m/s2), and that the ball has no initial velocity (the ball is not moving to start). Have the program output the height of the ball above the ground after 0, 1, 2, 3, 4, and 5 seconds. The ball should not go underneath the ground (height 0).
+
+// Use a function to calculate the height of the ball after x seconds. The function can calculate how far the ball has fallen after x seconds using the following formula: distance fallen = gravity_constant * x_seconds2 / 2
