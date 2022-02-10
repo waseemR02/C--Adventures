a953a1dc467f8c061529e125d7bd9a9d0438e5d2
waseemR02 <waseem.riaz.9999@gmail.com>
2022-02-10 12:17:30 +0530

    Rudimentary Calculator

--- Problems/Problem-1.cpp
+++ Problems/Problem-1.cpp
@@ -45,7 +45,4 @@ int main()
     return 0;
 }
 
-/*Write the following program: The user is asked to enter 2 floating point numbers (use doubles).
-The user is then asked to enter one of the following mathematical symbols: +, -, *, or /.
-The program computes the answer on the two numbers the user entered and prints the results.
-If the user enters an invalid symbol, the program should print nothing.*/
+/*Write the following program: The user is asked to enter 2 floating point numbers (use doubles).The user is then asked to enter one of the following mathematical symbols: +, -, *, or /.The program computes the answer on the two numbers the user entered and prints the results.If the user enters an invalid symbol, the program should print nothing.*/
