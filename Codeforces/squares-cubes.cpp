670d90dfddfae55776cf3ea71d71e55181b45d48
waseemR02 <waseem.riaz.9999@gmail.com>
2022-02-20 14:17:22 +0530

    Codeforces

---
+++ Codeforces/squares-cubes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int counter(int n)
+{
+    int count{0};
+
+    for(int i{1}; i*i < n+1; i++)
+    
+        count++;
+    
+
+    for(int i{1}; i*i*i < n+1; i++)
+    {
+        count++;
+    }
+
+    for(int i{1}; i*i*i*i*i*i < n+1; i++)
+    {
+        count--;
+    }
+    
+    return count;
+}
+
+
+int main()
+{   
+    int i{};
+
+    std::cin >> i;
+
+    while(std::cin >> i)
+    { int count =counter(i);
+        std::cout << count << std::endl; 
+    }
+}
