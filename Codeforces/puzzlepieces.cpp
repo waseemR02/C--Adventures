72d423db546a38ef7f448f7827eb1e7080a8ad20
waseemR02 <waseem.riaz.9999@gmail.com>
2022-03-11 09:25:35 +0530

    Puzzle Pieces

---
+++ Codeforces/puzzlepieces.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n,m;
+        cin >> n >> m;
+
+        if(n == 1 || m == 1 || (n == 2 && m == 2))
+            cout << "YES\n";
+        else
+            cout << "NO\n"; 
+    }
+
+    return 0;
+}
